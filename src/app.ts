54fa8a377f440566b03a73d92e78e7b8ab28e22b
mohammedazizul <mohammedazizulhoquesheikh@gmail.com>
2023-05-31 22:29:23 +0800

    create user route completed

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
+
+
 
 const app: Application = express()
 
@@ -9,9 +12,25 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application routes 
+app.use('/api/v1/users/', usersRouter)
+
 // test
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+
+// test
+// import usersService from './app/modules/users/users.service'
+// app.get('/', async (req: Request, res: Response) => {
+  
+//   await usersService.createUser({
+//     id: '999',
+//     password: "1234",
+//     role: "student"
+//   })
+
+//   res.send('Hello World!')
+// })
 
 export default app
